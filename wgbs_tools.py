aeab1c6c37f9d24ef0a3931e709d82e3a3390482
Cherry-Kim <64776690+Cherry-Kim@users.noreply.github.com>
2023-10-20 14:53:15 -0700

    Update wgbs_tools.py

--- wgbs_tools.py
+++ wgbs_tools.py
@@ -2,3 +2,30 @@
 #Genome configuration
 #wgbstools init_genome hg19
 
+import string,sys,os
+import subprocess
+
+def step1():
+    region, co = [], 0
+    sample = 'GSM6810004_CNVS-NORM-110000264-cfDNA-WGBS-Rep1.pat.gz'
+    fp = open('region2.txt','r') #chr13:27928900-27929266
+    for line in fp:
+        if line.strip():
+            region.append(line.strip())
+    fp.close()
+
+    with open('GSM6810004','a') as output_file:
+        for i in region:
+            co += 1
+            print("Region",co," ",i)
+
+            command = "/BIO5/wgbs_tools/wgbstools vis GSM6810003_CNVS-NORM-110000263-cfDNA-WGBS-Rep1.pat.gz -r "+i
+            process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            stdout, stderr = process.communicate()
+
+            if 'Methylation' in stdout.decode():
+                output_lines = [line for line in stdout.decode().split('\n') if 'Methylation average:' in line]
+                output_file.write('\n'.join(output_lines)+'\n')
+            else:
+                output_file.write('No results\n')
+
